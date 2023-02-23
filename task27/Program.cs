de7617dcedc656165d7f67c32a51bb0dbf3c7531
eastdrop <lyokha7777@gmail.com>
2023-02-23 12:02:10 +0300

    added task27

---
+++ task27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Sum(int a)
+{
+    int res = 0;
+    while (a > 0)
+    {
+        int b = a % 10;
+        a = a / 10;
+        res = res + b;
+    }
+    Console.WriteLine(res);
+}
+int N = ReadInt("Input Number: ");
+Sum(N);
