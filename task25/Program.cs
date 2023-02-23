c0c03e468342462f5d15eb43ce4801315133b2db
eastdrop <lyokha7777@gmail.com>
2023-02-23 11:49:55 +0300

    added task25

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4-> 16*/
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void AinB(int number1, int number2)
+{
+    Console.WriteLine(Math.Pow(number1, number2));
+}
+int A = ReadInt("Введите число A: ");
+int B = ReadInt("Введите число B: ");
+AinB(A, B);
